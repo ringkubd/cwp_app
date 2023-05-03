2e470c8ceced46b502581dc102b9c7227e0742ee
Anwar Jahid <ajr.jahid@gmail.com>
2023-05-03 17:57:44 +0600

    local

--- screens/Home.js
+++ screens/Home.js
@@ -2,31 +2,17 @@ import {Pressable, Text, View} from "react-native";
 import AuthLayout from "../layout/authLayout";
 import {Button} from "react-native-paper";
 import {useDispatch} from "react-redux";
-import {useServerTypeMutation} from "../services/rtkQuery/serverApi";
+import {useAccountListQuery, useServerTypeMutation} from "../services/rtkQuery/serverApi";
 import {useEffect} from "react";
 
 export default function Home({navigation}){
     const dispatch = useDispatch();
     const [submit, result] = useServerTypeMutation();
+    const getAccountList = useAccountListQuery();
     useEffect(() => {
-        const formData = new FormData();
-        formData.append("key", "N42nU3lRfA4ozmylnUfDHn9mcS0fbmB3T4meS3VX8qXAraQDdGtg7Rb1eawZccZ1VX9oco");
-        formData.append("action", "list");
-
-        let myHeaders = new Headers();
-        myHeaders.append("Accept", "application/json");
-        let requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: formData,
-            redirect: 'follow'
-        };
-
-        fetch("https://5.189.130.79:2304/v1/typeserver", requestOptions)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
+        submit();
     }, [])
+    console.log(result, getAccountList)
 
     return (
         <AuthLayout>
--- services/rtkQuery/serverApi.js
+++ services/rtkQuery/serverApi.js
@@ -1,45 +1,66 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import {navigate} from "../../utils/navigate";
 
+const rawBaseQuery = fetchBaseQuery({
+    baseUrl: '',
+    prepareHeaders: headers => {
+        headers.set('content-type', 'multipart/form-data');
+        return headers;
+    }
+})
+
+const dynamicBaseQuery = async (args, api, extraOptions) => {
+    const apiBase = api.getState()?.server?.api_base
+    if (!apiBase) {
+        return {
+            error: {
+                status: 400,
+                statusText: 'Bad Request',
+                data: 'No project ID received',
+            },
+        }
+    }
+    const urlEnd = typeof args === 'string' ? args : args.url
+    args.body.key = api.getState()?.server?.api_key
+    const formData = new FormData();
+    for ( let key in args.body ) {
+        formData.append(key, args.body[key]);
+    }
+    args.body =  formData;
+    console.log(args)
+    // construct a dynamically generated portion of the url
+    const adjustedUrl = apiBase+`${urlEnd}`
+    const adjustedArgs = typeof args === 'string' ? adjustedUrl : { ...args, url: adjustedUrl }
+    // provide the amended url and other params to the raw base query
+    return rawBaseQuery(adjustedArgs, api, extraOptions)
+}
+
 export const ServerApi = createApi({
     reducerPath: 'ServerApi',
-    baseQuery: async (args, api, extraOption) => {
-        const apiBase = api.getState()?.server?.api_base
-        if (!apiBase) {
-            return {
-                error: {
-                    status: 400,
-                    statusText: 'Bad Request',
-                    data: 'No project ID received',
-                },
-            }
-        }
-        const adjustedArgs = typeof args === 'string' ? apiBase : { ...args, url: apiBase }
-        const b = fetchBaseQuery({
-            baseUrl: apiBase,
-            prepareHeaders: (headers, {getState}) => {
-                headers.set('Accept', `application/json`);
-                headers.set('Content-Type', 'application/json')
-                return headers
-            }
-        });
-        console.log(await  b(adjustedArgs, api, extraOption))
-        return b(adjustedArgs, api, extraOption);
-    },
+    baseQuery: dynamicBaseQuery,
     tagTypes: [],
     endpoints: builder => ({
         serverType: builder.mutation({
             query: (arg) => ({
-                url: '/account_metadata/',
+                url: 'typeserver',
                 method: 'POST',
-                formData: {
+                body: {
                     action: 'list',
-                    key: "abc#@!"
                 },
                 responseType: 'json'
             })
-        })
+        }),
+        accountList: builder.query({
+            query: (arg) => ({
+                url: 'account',
+                method: 'POST',
+                body: {
+                    action: 'list',
+                },
+                responseType: 'json'
+            })
+        }),
     })
 })
 
-export const { useServerTypeMutation } = ServerApi;
+export const { useServerTypeMutation, useAccountListQuery } = ServerApi;
